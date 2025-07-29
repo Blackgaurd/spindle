28d8a7402d9579fcc99c74b8860d53fac28f3907
Bryan Deng <bryan.deng002@gmail.com>
2025-07-29 23:09:33 +0000

    Add example for probabalistic math grammar

---
+++ examples/probabilistic_math.rs
@@ -0,0 +1,25 @@
+mod common;
+use spindle_lib::Grammar;
+
+use crate::common::rand_u;
+
+fn main() {
+    // this grammar will generate more numbers than expressions
+    let grammar: Grammar = r#"
+        expr   : num                     90 
+               | paren                   5
+               | expr symbol expr        5  ;
+        paren  : "(" expr symbol expr ")"   ;
+        symbol : r"-|\+|\*|÷"               ;
+        num    : r"[0-9]+"                  ;
+    "#
+    .parse()
+    .unwrap();
+
+    let mut buf = [0; 4096];
+    let mut u = rand_u(&mut buf);
+    for _ in 0..100 {
+        let sentence: String = grammar.expression(&mut u, Some(10)).unwrap();
+        println!("{}\n", sentence);
+    }
+}
