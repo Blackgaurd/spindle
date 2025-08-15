5d61d407f2688c5d435fce9f00b8e8496801114f
Bryan Deng <bryan.deng002@gmail.com>
2025-08-14 23:47:25 -0700

    fix grammar  in probabalistic math example

--- examples/probabilistic_math.rs
+++ examples/probabilistic_math.rs
@@ -6,9 +6,9 @@ use crate::common::rand_u;
 fn main() {
     // this grammar will generate more numbers than expressions
     let grammar: Grammar = r#"
-        expr   : num                     90 
-               | paren                   5
-               | expr symbol expr        5  ;
+        expr   : num                   @ 90
+               | paren                 @ 5
+               | expr symbol expr      @ 5  ;
         paren  : "(" expr symbol expr ")"   ;
         symbol : r"-|\+|\*|÷"               ;
         num    : r"[0-9]+"                  ;
